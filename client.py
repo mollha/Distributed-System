7368b7710732d7c9f593e6305327b28400845f1b
mollha <mollyxhayward@gmail.com>
2019-02-20 23:13:24 +0000

    Small config changes

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
+# Client.py
+
 import Pyro4
 
 uri = input("What is the Pyro uri of the Hello object? ").strip()
 name = input("What is your name? ").strip()
 
-Hello = Pyro4.Proxy(uri)
-print("Response: ", Hello.say_hello(name))
+Hello = Pyro4.Proxy(uri)        # get a Pyro proxy to the Hello object
+print("Response: ", Hello.say_hello(name))      # call a remote method
--- front_end_server.py
+++ front_end_server.py
@@ -5,13 +5,14 @@ with Pyro4.locateNS() as name_server:
     server_dict = name_server.list(prefix="ratings.database.")
 
 
-
 num_servers = len(server_dict.keys())
 
 servers = server_dict.values()
 server_list = []
+
+
 def create_servers(server_dict):
 
-for item in server_dict.values():
-    server = Pyro4.Proxy(item)
-    server_list.append(server)
+    for item in server_dict.values():
+        server = Pyro4.Proxy(item)
+        server_list.append(server)
